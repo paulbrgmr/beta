8b8c2bbeade59636c7aef722fd746510cdb9476b
Paul <Paul Bergmeir>
2015-06-20 12:55:11 +0200

    made by fix

--- sepp/_resources/js/own.js
+++ sepp/_resources/js/own.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('#navbar').load('navbar.html');
     $('#formular').load('formular.php');
-    $('#made-by').load('Responsive Website Umsetzung:<br><a href="http://www.pbergmeir.com" target="_blank">Paul Bergmeir</a>');
+    $('#made-by').html('Responsive Website Umsetzung:<br><a href="http://www.pbergmeir.com" target="_blank">Paul Bergmeir</a>');
     $('.slick-slider').slick({
         accessibility: true,
         dots: true
