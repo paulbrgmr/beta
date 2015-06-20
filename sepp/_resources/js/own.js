7ac4bc03a00604d2846302657def578c96de95bd
Paul <Paul Bergmeir>
2015-06-20 13:01:12 +0200

    footer

--- sepp/_resources/js/own.js
+++ sepp/_resources/js/own.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('#navbar').load('navbar.html');
     $('#formular').load('formular.php');
     $('#made-by').html('Responsive Website Umsetzung:<br><a href="http://www.pbergmeir.com" target="_blank">Paul Bergmeir</a>');
-    $('body').prepend(function() {
+    $('body').prependTo(function() {
         $(this).load('footer.html');
     });
     $('.slick-slider').slick({
