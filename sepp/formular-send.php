80cdb3b29ac2ec0bb7cdf239bc14c105b1f8fd30
Paul <Paul Bergmeir>
2015-06-20 14:05:22 +0200

    footer added to other pages

--- sepp/formular-send.php
+++ sepp/formular-send.php
@@ -113,6 +113,8 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
         </form>
     </div>
 
+            <footer></footer>
+
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <!-- Include all compiled plugins (below), or include individual files as needed -->
